cf7d1b3a52b5b577067bd25098d0b54f57df6cc6
Megha Vijay <mvijay@sapient.com>
2018-02-27 18:56:24 +0530

    images added

--- app/components/DeviceRow.jsx
+++ app/components/DeviceRow.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { withStyles } from 'material-ui/styles';
 
 import style from '../css/components/device-row';
 import classNames from 'classnames/bind';
+
+import Android from '../images/android.png';
+import IOs from '../images/ios.png';
+import Windows from '../images/windows2.png';
 
 const cx = classNames.bind(style);
 
 
 
 const DeviceRow = ({devices, deviceOS}) => {
-	const android_url = "https://lh3.googleusercontent.com/TS1V-D1PkLTyFkjdmGQKHnv4CE7XgSHolKpQM9sD6hb2LtSf04ToahCJa3go-g4dQ3A=w300-rw";
-	const ios_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/IOS_logo.svg/300px-IOS_logo.svg.png";
-	const windows_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs-w6tLNeZrGk6guNJ0J-bs8bZq4D5xaktCHZfDoCcLeQaq5ZYLw";
 	return (
 		<div className={cx('root')}>
 			<div className={cx('img-container')}> 
 			<img className={cx('os-logo')} 
-				src={deviceOS === "Android"?android_url:(deviceOS==="iOS"?ios_url:windows_url)} />
+				src={deviceOS === "Android"?Android:(deviceOS==="iOS"?IOs:Windows)} />
 			</div>
 			<div className = {cx('os-devices')}>
 				{devices.map((device, index) => {
