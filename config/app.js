e3af55f5bba4a68eb03fd8b4471ffce9d2322dcb
nawazsk <nshaik2@sapient.com>
2018-03-16 18:26:10 +0530

    ip replaced with localhost

--- config/app.js
+++ config/app.js
@@ -4,7 +4,7 @@ export const isProduction = ENV === 'production';
 export const isDebug = ENV === 'development';
 export const isClient = typeof window !== 'undefined';
 
-export const apiEndpoint = isDebug ? 'http://10.150.194.123:3000' : 'https://device-hub.herokuapp.com';
+export const apiEndpoint = isDebug ? 'http://localhost:3000' : 'https://device-hub.herokuapp.com';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
 
